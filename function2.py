f2ef2b790a1e2efd04b9afa255013d8fd137c74f
sachin <sachins@sourcesoftsolutions.com>
2020-11-05 00:39:28 +0530

    change in function 3

--- function2.py
+++ function2.py
@@ -1,6 +1,7 @@
-def printer(str):
-    print(str)
+def printer(a,b,c):
+    print(a,c,b)
     return
-printer("how are you")
+#printer("how are you")
 "This is a test "
 "Does not matter here if we have return or not"
+printer(3,4,7)
