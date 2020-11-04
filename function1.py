0acb9b996218cb3b3cb7a1411af092d097aef93b
sachin <sachins@sourcesoftsolutions.com>
2020-11-04 21:49:17 +0530

    adding the function1 and 2

---
+++ function1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# Function definition is here
+def printme( str ):
+    #"This prints a passed string into this function"
+    print (str)
+    return
+# Now you can call printme function
+printme("This is first call to the user defined function!")
+printme("Again second call to the same function")
+printme("hello sachin, this is 3rd call of the function")
+printme(2+3)
+printme("sachin")
+
+
+#Output:
+#This is first call to the user defined function!
+#Again second call to the same function
+#hello sachin, this is 3rd call of the function
+#5
+#sachin
