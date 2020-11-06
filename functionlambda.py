4cb194b924d7494e0e67b40c5eea2b88975b2db2
sachin <sachins@sourcesoftsolutions.com>
2020-11-07 03:52:29 +0530

    return added

---
+++ functionlambda.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+#function definition is here
+sum = lambda arg1,arg2,arg3:arg1+arg2+arg3
+
+#Now you can call sum as a function
+print("value of total is : ",sum(10,20,30))
---
+++ return.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def test(i,j):
+    #return i+j #output will be 8
+    z=i+j  #if we leave the code here without return output will be blank
+    return z #it will return 8
+print(test(3,5))
