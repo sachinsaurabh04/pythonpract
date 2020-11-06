0d2438e11c606f44f12dfd241d0ce5015ffe01e3
sachin <sachins@sourcesoftsolutions.com>
2020-11-07 04:32:54 +0530

    adding module dir

---
+++ Module Example/calculator.py
@@ -0,0 +1,2 @@
+import fullcalcfile as cal
+https://www.youtube.com/watch?v=7GXaobCrBb4
---
+++ Module Example/fullcalcfile.py
@@ -0,0 +1,11 @@
+def add(a,b):
+    return a+b
+
+def add(a,b):
+    return a-b
+
+def add(a,b):
+    return a*b
+
+def add(a,b):
+    return a/b
