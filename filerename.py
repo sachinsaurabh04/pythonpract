fb2009bbbf66d4a32b6ffcb51093511b42e4ece6
sachin <sachins@sourcesoftsolutions.com>
2020-12-08 00:14:47 +0530

    update in file rename

---
+++ filerename.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import os
+#renaming the file named foo.txt to foo1.txt
+#os.rename('foo.txt', 'foo1.txt')
+#to delete the file name foo.txt
+#os.remove('foo.txt')
+
+#Below code tells how to read a complete file and close it.
+#fo=open('foo1.txt',"r+")
+#str= fo.read()
+#print(str)
+#fo.close()
+
+#to see the present working directory
+#os.getcwd()
+#to delete any directory
+#os.rmdir('dirname')
