077766a09d590b993607ecbec619b8667ca11419
sachin <sachins@sourcesoftsolutions.com>
2020-12-28 22:03:42 +0530

    commit on 28dec2020

--- Object Oriented/class2.py
+++ Object Oriented/class2.py
@@ -21,3 +21,13 @@ print("Employee.__name__:", Employee.__name__)
 print("Employee.__module__:", Employee.__module__)
 print("Employee.__bases__:", Employee.__bases__)
 print("Employee.__dict__:", Employee.__dict__)
+
+
+"""
+/home/sachin/PycharmProjects/pythonProject1/venv/bin/python "/home/sachin/PycharmProjects/pythonProject1/Object Oriented/class2.py"
+Employee.__doc__: Common base class for all employees
+Employee.__name__: Employee
+Employee.__module__: __main__
+Employee.__bases__: (<class 'object'>,)
+Employee.__dict__: {'__module__': '__main__', '__doc__': 'Common base class for all employees', 'empCount': 2, '__init__': <function Employee.__init__ at 0x7f2b0c7cbf70>, 'displayCount': <function Employee.displayCount at 0x7f2b0c7db040>, '__dict__': <attribute '__dict__' of 'Employee' objects>, '__weakref__': <attribute '__weakref__' of 'Employee' objects>}
+"""
